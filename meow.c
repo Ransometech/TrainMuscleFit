2e73c8d85d7dedd0ac7316faf2e8886fcedd8f2e
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-07 22:35:53 +0000

    Sun, Jul 7, 2024, 10:35 PM +00:00

--- meow.c
+++ meow.c
@@ -2,11 +2,10 @@
 
 int main(void)
 {
-   int i = 0;
-   while(i < 3)
-   {
-    printf("meow/n");
-    i++;
-
-   }
+    int i = 1;
+    while (i <= 3)
+    {
+        printf("meow\n");
+        i++;
+    }
 }
