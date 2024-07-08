427fafe2709db51f12a8e03042b69fc0846d132b
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-08 21:15:20 +0000

    Mon, Jul 8, 2024, 10:15 PM +01:00

--- mario.c
+++ mario.c
@@ -7,15 +7,8 @@ int main(void)
     do
     {
         n = get_int("Size: ");
-    }`
+        printf(n)
+    }
     while (n < 1);
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
 }
