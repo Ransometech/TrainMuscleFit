1702f2b67bda7b5d1b44feb21319a77103a300ad
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 20:44:06 +0000

    Thu, Jul 11, 2024, 9:44 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -18,7 +18,7 @@ int main(int argc, string argv[])
     }
     else if (strlen(argv[1]) != 26)
     (
-        printf("Usage: ./substitution key\n");
+        printf("Key must contain 26 characters\n");
 
     )
 
