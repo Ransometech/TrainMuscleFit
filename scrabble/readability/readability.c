ec377994959efc846b4fe827453b98845a06b0ec
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 15:54:16 +0000

    Thu, Jul 11, 2024, 4:54 PM +01:00

---
+++ scrabble/readability/readability.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+
+int main(void)
+{
+    // Prompt the user for some text
+    string text = get_string("Text: ");
+
+    // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
+
+    // Compute the Coleman-Liau index
+
+    // Print the grade level
+}
+
+int count_letters(string text)
+{
+    // Return the number of letters in text
+}
