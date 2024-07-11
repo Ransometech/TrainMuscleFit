ccbfd8ca00040fdcd7fbafc873f7ad2151f02428
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 19:03:39 +0000

    Thu, Jul 11, 2024, 8:03 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -11,15 +11,9 @@ int count_sentences(string text);
 
 int main(int argc, string argv[])
 {
-    if (argc != 2 )
+    if (argc != 2  || !isdigit(argc[1]))
     {
         printf("Usage: ./caesar key\n");
-        return 1;
-
-    }
-    if (!isdigit(argc[1]))
-    {
-        printf("Usage: ./caesar2 key\n");
         return 1;
 
     }
