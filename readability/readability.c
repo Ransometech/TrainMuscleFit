97e46be9b593bbae2e86142659c8280e686f1168
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 17:13:55 +0000

    Thu, Jul 11, 2024, 6:13 PM +01:00

---
+++ readability/readability.c
@@ -0,0 +1,75 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+// Function prototypes
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    // Prompt the user for some text
+    string text = get_string("Text: ");
+
+    // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
+    float words = count_words(text);
+    int sentences = count_sentences(text);
+
+    printf("The text contains %d letters.\n", letters);
+    printf("The text contains %d sentences.\n", sentences);
+
+    // Compute the Coleman-Liau index
+    float L = (letters/words) * 100;
+    float S = (sentences/words) * 100;
+    int index = round( 0.0588 * L - 0.296 * S - 15.8);
+
+    // Print the grade level
+    printf("Grade %d", index);
+}
+
+int count_letters(string text)
+{
+
+    // Return the number of letters in text
+    int count = 0;
+    for (int i=0, text_len = strlen(text); i<text_len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(string text)
+{
+    // Return the number of words in text
+    int count = 0;
+    for (int i=0, text_len=strlen(text); i<text_len; i++)
+   {
+        if (text[i] == ' ')
+        {
+            count++;
+        }
+    }
+    return count + 1;
+}
+
+int count_sentences(string text)
+{
+    // Return the number of sentences in text
+   int count = 0;
+    for (int i=0, text_len=strlen(text); i<text_len; i++)
+   {
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            count++;
+        }
+    }
+    return count;
+}
