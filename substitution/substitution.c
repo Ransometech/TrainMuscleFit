49a31a701f31ab5ca439bef7fc9d67da87f8d27a
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 21:34:37 +0000

    Thu, Jul 11, 2024, 10:34 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -71,7 +71,7 @@ bool not_repeat(string key)
     {
         for (int j = 1; j < len; j++)
         {
-            if (key[j+i]==key[i])
+            if (key[j+i] == key[i])
             {
                 printf("%c %c", key[j], key[i]);
 
