4108fc75dd38beabc7aa9eb8d45df190d393597c
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 18:29:32 +0000

    Thu, Jul 11, 2024, 7:29 PM +01:00

---
+++ caesar/caesar.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+// Function prototypes
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+
+    
+}
+
