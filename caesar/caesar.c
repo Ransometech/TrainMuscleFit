d1979b01d65257845802db39e1019d9fb03d76b4
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 19:11:43 +0000

    Thu, Jul 11, 2024, 8:11 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,7 +10,7 @@ bool is_digits(string s);
 
 int main(int argc, string argv[])
 {
-    if (argc != 2  || !isdigit(argv[1]))
+    if (argc != 2 || !is_digits(argv[1]))
     {
         printf("Usage: ./caesar key\n");
         return 1;
@@ -20,11 +20,11 @@ int main(int argc, string argv[])
 
 }
 
-bool is_digits(string s)
+bool is_digits(string key)
 {
-    for (int i = 0, len = strlen(s); i < len; i++)
+    for (int i = 0, len = strlen(key); i < len; i++)
     {
-        if (!isdigit(s[i]))
+        if (!isdigit(key[i]))
         {
             return false;
         }
