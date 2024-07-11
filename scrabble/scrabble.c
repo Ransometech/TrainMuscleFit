9b6cc49d3255581c6c3d8a2ae68b0c427baa36d1
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 12:00:13 +0000

    Thu, Jul 11, 2024, 1:00 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,9 +3,17 @@
 #include <stdio.h>
 #include <string.h>
 int char
+
 int main(void)
 {
+    // Prompt the Players for two words
     string word1 = get_string("Player 1: ")
-    string word2 = get_string("Player: ")
+    string word2 = get_string("Player 2: ")
+
+     // Compute the score of each word
+
+      // Print the winner
+
+
 
 }
