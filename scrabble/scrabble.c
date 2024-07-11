a69bc4d6f60b021d038838e97dd0f92304984df7
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 12:59:44 +0000

    Thu, Jul 11, 2024, 1:59 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 
+// Function prototype
 int calculate_score(string word);
+
+// Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
@@ -38,7 +41,6 @@ int calculate_score(string word)
     int wlen = strlen(word);
 
     // Calculate score for each character
-
     for (int i = 0; i < wlen; i++)
     {
         if (isupper(word[i]))
