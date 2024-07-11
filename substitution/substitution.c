95c737153a67cb26e9f287dabbb918cc243f0b23
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-11 20:52:15 +0000

    Thu, Jul 11, 2024, 9:52 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -17,20 +17,24 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    if (strlen(argv[1]) != 26)
-    (
+    else if (strlen(argv[1]) != 26)
+    {
         printf("Key must contain 26 characters\n");
         return 1;
 
-    )
-
-     int key = atoi(argv[1]);
+    }
+    
+    else
+    {
+        int key = atoi(argv[1]);
         string plaintext = get_string("Plaintext: ");
 
         // Encrypt the plaintext
         ciphertext(plaintext, key);
+    }
 
-    
+
+
 
 
 }
