cd4c9e4402b028c5285e3b5b55c59d14cd0bb0be
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-13 19:08:40 +0000

    Sat, Jul 13, 2024, 8:08 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -104,7 +104,7 @@ void bubble_sort(void)
 {
     for (int i=0; i<candidate_count-1;i++)
     {
-        for (int j =0; j < candidate_count-2; j++)
+        for (int j =0; j < candidate_count-i-1; j++)
         {
             if (candidates[j].votes < candidates[j+1].votes)
             {
