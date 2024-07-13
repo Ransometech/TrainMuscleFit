cb65b9c53529c853f53a3fcacdf367976cf0b564
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-13 23:53:08 +0000

    Sun, Jul 14, 2024, 12:53 AM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -198,9 +198,16 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // Check if eliminated and has a different vote count than min
+        if (!candidates[i].eliminated && candidates[i].votes != min)
+        {
+            return false;
+        }
+    }
 
-    return false;
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
