6b02e04e3930f8f4ac8bc25ca5c5033dd4bfe08b
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-15 19:49:38 +0000

    Mon, Jul 15, 2024, 8:49 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,24 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+     // Convert each pixel to grayscale
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Get the pixel
+            RGBTRIPLE *pixel = &image[i][j];
+
+            // Calculate average color
+            int average = (pixel->rgbtRed + pixel->rgbtGreen + pixel->rgbtBlue) / 3;
+
+            // Assign the average color to the pixel
+            pixel->rgbtRed = average;
+            pixel->rgbtGreen = average;
+            pixel->rgbtBlue = average;
+        }
+    }
+
     return;
 }
 
@@ -15,7 +33,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+
     return;
 }
 
