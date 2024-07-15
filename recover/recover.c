454bf769d00aafc492db2aea2e29f5603a9fdddb
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-15 23:41:42 +0000

    Tue, Jul 16, 2024, 12:41 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,43 +17,11 @@ int main(int argc, char *argv[])
     // Create a buffer for a block of data
     uint8_t buffer[512];
 
-    FILE *img = NULL;
-    int file_count = 0;
-    
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
-        // Close the previous JPEG if it exists
-        if (img != NULL)
-        {
-            fclose(img);
-        }
-
-        // Create a new JPEG file
-        char filename[8];
-        sprintf(filename, "%03i.jpg", file_count);
-        img = fopen(filename, "w");
-        if (img == NULL)
-        {
-            printf("Could not create file.\n");
-            return 1;
-        }
-        file_count++;
-    }
-
-        // Write to the JPEG file if it has been opened
-        if (img != NULL)
-        {
-            fwrite(buffer, 1, 512, img);
-        }
-    }
-
-    // Close any remaining open files
-    if (img != NULL)
-    {
-        fclose(img);
-    }
-    fclose(card);
+        // Create JPEGs from the data
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0){
 
 
         }
