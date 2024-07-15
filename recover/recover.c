62e4d49f756ad1ede148d83b9fde7374c80034f5
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-15 23:39:54 +0000

    Tue, Jul 16, 2024, 12:39 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,6 +17,9 @@ int main(int argc, char *argv[])
     // Create a buffer for a block of data
     uint8_t buffer[512];
 
+    FILE *img = NULL;
+    int file_count = 0;
+    
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
