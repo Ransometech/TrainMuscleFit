970fe75b21ecbe2f7064f708b82e3202cd8f4ff7
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-15 19:50:51 +0000

    Mon, Jul 15, 2024, 8:50 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -27,13 +27,45 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+     // Apply sepia filter to the image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Get the original colors
+            RGBTRIPLE *pixel = &image[i][j];
+            int originalRed = pixel->rgbtRed;
+            int originalGreen = pixel->rgbtGreen;
+            int originalBlue = pixel->rgbtBlue;
+
+            // Calculate new sepia colors
+            int sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
+            int sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
+            int sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
+
+            // Assign sepia colors to pixel
+            pixel->rgbtRed = sepiaRed;
+            pixel->rgbtGreen = sepiaGreen;
+            pixel->rgbtBlue = sepiaBlue;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-
+    // Reflect the image horizontally
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap the pixels
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
     return;
 }
 
