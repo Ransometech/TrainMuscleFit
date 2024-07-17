ead993f31e956b716496539085338a3af470a837
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-17 19:47:38 +0000

    Wed, Jul 17, 2024, 8:47 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -58,7 +58,6 @@ person *create_family(int generations)
         new_person->parents[0] = parent0;
         new_person->parents[1] = parent1;
 
-
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         new_person->alleles[0] = parent0->alleles[rand() % 2];
         new_person->alleles[1] = parent1->alleles[rand() % 2];
@@ -115,11 +114,13 @@ void print_family(person *p, int generation)
     // Print person
     if (generation == 0)
     {
-        printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0],
+               p->alleles[1]);
     }
     else if (generation == 1)
     {
-        printf("Parent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Parent (Generation %i): blood type %c%c\n", generation, p->alleles[0],
+               p->alleles[1]);
     }
     else
     {
@@ -127,7 +128,8 @@ void print_family(person *p, int generation)
         {
             printf("Great-");
         }
-        printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0],
+               p->alleles[1]);
     }
 
     // Print parents of current generation
