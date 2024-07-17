01a2e76be439ada5ea8d298025e81020ff140bae
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-17 18:00:54 +0000

    Wed, Jul 17, 2024, 7:00 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,7 +49,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new_person->parent0 = 
+        new_person->parent0 = parent[0];
+        new_person->parent1 = parent[1];
 
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
