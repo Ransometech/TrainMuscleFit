0ce0464482a58538965debc95e72a427aa321196
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-18 15:45:16 +0000

    Thu, Jul 18, 2024, 4:45 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,5 +1,3 @@
-// Implements a dictionary's functionality
-
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -8,25 +6,31 @@
 
 #include "dictionary.h"
 
-int count = 0; // Counter for the number of words in the dictionary
+int count = 0;
 
-// Represents a node in a hash table
 typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
 } node;
 
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 10000;
+const unsigned int N = 10000; // Increase number of buckets for better distribution
 
-// Hash table
 node *table[N];
 
-// Returns true if word is in dictionary, else false
+unsigned int hash(const char *word)
+{
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *word++))
+    {
+        hash = ((hash << 5) + hash) + toupper(c); // hash * 33 + c
+    }
+    return hash % N;
+}
+
 bool check(const char *word)
 {
-    // TODO
     char allupper_word[LENGTH + 1];
     for (int i = 0; word[i] && i < LENGTH; i++)
     {
@@ -34,7 +38,6 @@ bool check(const char *word)
     }
     allupper_word[strlen(word)] = '\0';
 
-    // Get index
     unsigned int index = hash(allupper_word);
     node *cursor = table[index];
 
@@ -49,76 +52,49 @@ bool check(const char *word)
     return false;
 }
 
-// Hashes word to a number
-unsigned int hash(const char *word)
-{
-    // TODO: Improve this hash function
-    unsigned int hash = 0;
-    while (*word)
-    {
-        hash = (hash << 2) ^ toupper(*word++);
-    }
-    return hash % N;
-}
-
-// Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
     FILE *dict_file = fopen(dictionary, "r");
-    if (dict_file == NULL){
-        printf("Can't open file");
-
+    if (dict_file == NULL)
+    {
+        printf("Can't open file\n");
         return false;
     }
     char word[LENGTH + 1];
-    // Read words from the file
+
     while (fscanf(dict_file, "%45s", word) != EOF)
     {
-        // new node
         node *n = malloc(sizeof(node));
-        if (n==NULL)
+        if (n == NULL)
         {
-
-            printf("Cant assign memory to new node");
+            printf("Can't assign memory to new node\n");
             return false;
         }
 
-        // Convert to uppercase
-        for(int i=0, word_len = strlen(word); i<word_len; i++)
+        int len = strlen(word);
+        for (int i = 0; i < len; i++)
         {
-            word[i] = toupper(word[i]);
+            n->word[i] = toupper(word[i]);
         }
+        n->word[len] = '\0';
 
-
-        // Copy word into the node
-        strcpy(n->word, word);
-        unsigned int index = hash(word);
-        // Get hash index
+        unsigned int index = hash(n->word);
         n->next = table[index];
         table[index] = n;
 
-       // printf("Inserted %s at index %u\n", word, index);
         count++;
     }
     fclose(dict_file);
     return true;
 }
 
-
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-
     return count;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    // Go through hash table
     for (int i = 0; i < N; i++)
     {
         node *cursor = table[i];
@@ -126,7 +102,6 @@ bool unload(void)
         {
             node *temp = cursor;
             cursor = cursor->next;
-            printf("Freeing %s\n", temp->word);
             free(temp);
         }
     }
