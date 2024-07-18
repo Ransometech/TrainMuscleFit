18f902c6535ba1546146b37e9b3b41cefa6664b8
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-18 14:02:04 +0000

    Thu, Jul 18, 2024, 3:02 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -99,5 +99,16 @@ unsigned int size(void)
 bool unload(void)
 {
     // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            printf("Freeing %s\n", temp->word);  // Print statement for visualization
+            free(temp);
+        }
+    }
+    return true;
 }
