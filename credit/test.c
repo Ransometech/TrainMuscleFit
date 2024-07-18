106c34d2cd4a5c1d0e912be30094edfbcb3c9221
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-18 11:30:43 +0000

    Thu, Jul 18, 2024, 12:30 PM +01:00

--- credit/test.c
+++ credit/test.c
@@ -1,54 +1,9 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int checksum(long number);
-
-int main(void)
+unsigned int improved_hash(const char *word)
 {
-    // Prompt the user for the credit card number
-    long number;
-    do
+    unsigned int hash = 0;
+    while (*word)
     {
-        number = get_long("Number: ");
+        hash = (hash << 2) ^ toupper(*word++);
     }
-    while (number < 1);
-
-    // Print the checksum
-    printf("%d\n", checksum(number));
-}
-
-int checksum(long number)
-{
-    int sum_digit = 0;
-    int sum_other_digit = 0;
-    int count = 0;
-
-    while (number > 0)
-    {
-        int digit = number % 10;
-        number /= 10;
-        count++;
-
-        // Check if the digit is in an odd position
-        if (count % 2 == 0)
-        {
-            int multiplied_digit = 2 * digit;
-
-            // If multiplied_digit is greater than 9, subtract 9
-            if (multiplied_digit > 9)
-            {
-                sum_other_digit = (multiplied_digit % 10) + (multiplied_digit / 10);
-            }
-
-
-        }
-        else
-        {
-            sum_digit += digit;
-        }
-    }
-
-    // Calculate and return the final checksum
-    int final_checksum = sum_digit + sum_other_digit;
-    return final_checksum;
+    return hash % N;
 }
