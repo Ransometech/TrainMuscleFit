d96f964a1a4442a5a4fd5c4519c136f79cae3a7d
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-18 14:57:45 +0000

    Thu, Jul 18, 2024, 3:57 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -53,7 +53,12 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    unsigned int hash = 0;
+    while (*word)
+    {
+        hash = (hash << 2) ^ toupper(*word++);
+    }
+    return hash % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
