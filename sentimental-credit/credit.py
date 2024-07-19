209e689abd7a931480a759b4650398134499c583
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 22:11:35 +0000

    Fri, Jul 19, 2024, 11:11 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -19,27 +19,21 @@ def checksum(number):
     card = str(number)
 
     for i in str(number):
-        # print("num", i,  number % 2,"_",  int(number /10),"_", number % 10)
 
         # get each digit and other digit
         other_digit.append(number % 10)
         number = int(number / 10)
         multiplied_digit.append(2 * (number % 10))
         number = int(number / 10)
-        print(multiplied_digit, "multiplied_digit")
-        print(other_digit, "other_digit")
         if number == 0:
             break
     # sum each digit of multiplied digit
     for i in multiplied_digit:
         sum_multiplied_digit += i % 10 + int(i/10)
-    print(sum_multiplied_digit)
 
     # sum each digit of other digit
     for i in other_digit:
         sum_other_digit += i
-    print(sum_other_digit)
-    print(20 % 10, "last")
 
     # check for invalid card
     if (sum_other_digit+sum_multiplied_digit) % 10 != 0:
