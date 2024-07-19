a043633cad10134ccf7ea432c35754bb27c61eb3
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 19:24:09 +0000

    Fri, Jul 19, 2024, 8:24 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,8 +3,16 @@ import re
 
 patterns  = [r'^\d{15}', r'^\d{16}', r'^\d{13}']
 
+def main():
+    while True:
+        number = get_int("Number: ")
+        if check_input(number):
+            break
+
+
 def check_input(number):
     if re.match(pattern, number):
         return True
     return False
+
 
