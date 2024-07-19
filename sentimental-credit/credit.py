c3801e911c969f4588bc288ecf805fa6a787b03e
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 19:27:04 +0000

    Fri, Jul 19, 2024, 8:27 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,5 +1,5 @@
 import re
-
+from cs50 import get_int
 
 patterns  = [r'^\d{15}', r'^\d{16}', r'^\d{13}']
 
@@ -11,8 +11,10 @@ def main():
 
 
 def check_input(number):
-    if re.match(pattern, number):
-        return True
+    for i in patterns:
+        if re.match(i, number):
+            return True
     return False
 
 
+main()
