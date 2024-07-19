8f251585348f88e918bbc27b15f9172a4bfec0a3
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 18:09:33 +0000

    Fri, Jul 19, 2024, 7:09 PM +01:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,17 +1,17 @@
 from cs50 import get_int
 
-height = get_int(" Height: ")
 
 
 def main():
+    height = get_int(" Height: ")
 
     for i in range(height):
-        print_row(i)
+        print_row(height, i)
 
 
-def print_row(height_row):
+def print_row(height, height_row):
         height = height+1
         space =   height - height_row
-        print ("#" * height_row + " " + "#" * height_row)
+        print (space * "#" * height_row + " " + "#" * height_row)
 
 main()
