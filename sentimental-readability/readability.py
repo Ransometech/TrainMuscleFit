cdaca36f41c35901c071a6a7cb1231bae46d5967
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 22:55:17 +0000

    Fri, Jul 19, 2024, 11:55 PM +01:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,16 @@
+
+
+def main():
+    pass
+
+
+def count_letters(text):
+    count = 0
+    for i in text:
+        if i.isalpha():
+            count +=1
+    return count
+
+
+def count_words(text):
+    for 
