c27e0b0bd3b56927ddcca03e8a5c536bf0be2c48
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 23:59:38 +0000

    Sat, Jul 20, 2024, 12:59 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -4,16 +4,18 @@ import sys
 
 def main():
 
+
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
         print("Invalid command-line usage")
         sys.exit(1)
 
     # TODO: Read database file into a variable
+    rows = []
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for row in reader:
-            print(row)
+            rows.append(row)
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
