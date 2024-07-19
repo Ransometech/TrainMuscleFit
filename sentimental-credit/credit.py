e0de9f6a87375d7481a99054cdf4438a7613ce7e
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 22:18:53 +0000

    Fri, Jul 19, 2024, 11:18 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -37,7 +37,7 @@ def checksum(number):
 
     # check for invalid card
     print (sum_other_digit+sum_multiplied_digit, "sum_other_digit+sum_multiplied_digit")
-    if (sum_other_digit+sum_multiplied_digit) % 10 != 0:
+    if (sum_other_digit+sum_multiplied_digit) % 10 != 0 or re.match(r'^5[1-5]\d+$' is False :
 
         print("INVALID")
 
