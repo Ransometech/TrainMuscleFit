8ca49ceb9466ca94c398c6657e111345a6639b4a
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 22:06:25 +0000

    Fri, Jul 19, 2024, 11:06 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,37 +1,25 @@
 import re
 from cs50 import get_int
 
-patterns  = [r'^\d{15}', r'^\d{16}', r'^\d{13}']
-
 def main():
     while True:
         number = get_int("Number: ")
-        if check_input(number):
+        if number > 0:
             break
     checksum(number)
-
-
-def check_input(number):
-    for i in patterns:
-        if re.match(i, str(number)):
-            return True
-    return False
-
 
 def checksum(number):
     multiplied_digit = []
     other_digit = []
     sum_multiplied_digit =  0
     sum_other_digit = 0
-    American_Express = [r'^34\d+$', r'^37\d+$']
-    MasterCard = [r'^51\d+$', r'^52\d+$', r'^53\d+$', r'^53\d+$', r'^54\d+$', r'^55\d+$']
-    Visa = r'^4\d+$'
     card = str(number)
 
 
     for i in str(number):
         #print("num", i,  number % 2,"_",  int(number /10),"_", number % 10)
 
+        
         other_digit.append(number % 10)
         number = int(number / 10)
         multiplied_digit.append(2 *(number % 10))
@@ -59,12 +47,6 @@ def checksum(number):
         print("VISA")
     else:
         print("AMEX")
-
-
-
-
-
-
 
 
 main()
