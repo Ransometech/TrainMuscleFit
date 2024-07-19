3eb8d45f47ae0eb6ec6f17b23dc06ac8b7f5a641
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 19:21:35 +0000

    Fri, Jul 19, 2024, 8:21 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,7 +1,10 @@
 import re
 
 
+patterns  = [r'^\d{15}', r'^\d{16}', r'^\d{13}']
 
-def check_input():
-    patterns  = [r'^\d{15}', r'^\d{16}', r'^\d{13}', r'^\d{15}'
-    ]
+def check_input(number):
+    if re.match(pattern, number):
+        return True
+    return False
+
