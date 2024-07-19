1b835d8ce31c4d6ea343ee842fc31e15f622ba5e
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 23:07:05 +0000

    Sat, Jul 20, 2024, 12:07 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,8 +2,21 @@
 
 def main():
     text =input("Text: ")
-    
 
+    letters = count_letters(text)
+    words = count_words(text)
+    sentence = count_sentences(text)
+
+    L = letters / words
+    S = letters / words
+    index = round(0.0588 * L - 0.296 * S -15.8)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif >16:
+        print("Grade 16+")
+    else:
+        print("Grade", index)
 
 def count_letters(text):
     count = 0
