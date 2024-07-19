5e22483907cf92a533113a4ec13819bc64b9bede
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 23:17:52 +0000

    Sat, Jul 20, 2024, 12:17 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,7 +3,7 @@
 def main():
 
     # Prompt the user for some text
-    text =input("Text: ")
+    text = input("Text: ")
 
     # Count the number of letters, words, and sentences in the text
     letters = count_letters(text)
@@ -13,40 +13,44 @@ def main():
     # Compute the Coleman-Liau index
     L = (letters / words) * 100
     S = (sentence / words) * 100
-    index = round(0.0588 * L - 0.296 * S -15.8)
+    index = round(0.0588 * L - 0.296 * S - 15.8)
 
     print(L, S, index)
 
     # Print the grade level
     if index < 1:
         print("Before Grade 1")
-    elif index>16:
+    elif index > 16:
         print("Grade 16+")
     else:
         print("Grade", index)
+
 
 def count_letters(text):
     count = 0
     for i in text:
         if i.isalpha():
-            count +=1
+            count += 1
     return count
 
+
 def count_words(text):
-    count =0
+    count = 0
     for i in text:
         if i == " ":
             count += 1
 
     return count + 1
 
+
 def count_sentences(text):
 
-    count =0
+    count = 0
     for i in text:
         if i in [".", "?", "!"]:
             count += 1
 
     return count
+
 
 main()
