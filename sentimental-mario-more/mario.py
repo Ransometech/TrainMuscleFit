7dbd0dca286d39ab662cc30093def9c76eb6fc4b
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-19 18:17:26 +0000

    Fri, Jul 19, 2024, 7:17 PM +01:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,23 +1,23 @@
 from cs50 import get_int
 
 
-
 def main():
 
-    // Prompt the user for the pyramid's height
+    # Prompt the user for the pyramid's height
     height = get_int(" Height: ")
 
+    # Print a pyramid of that height
     for i in range(height):
         print_row(height, i)
 
 
 def print_row(height, height_row):
-        height_row = height_row +1
-        space_len =   height - height_row
-        space = "*" * space_len
+    height_row = height_row + 1
+    space_len = height - height_row
+    space = " " * space_len
 
-        # Print a row of bricks for the left and right pyramid
+    # Print a row of bricks for the left and right pyramid
+    print(space + "#" * height_row + "  " + "#" * height_row)
 
-        print (space + "#" * height_row + "  " + "#" * height_row)
 
 main()
