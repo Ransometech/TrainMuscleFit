739e9de1854400fc48b891c90a1ea12af25878e1
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-21 23:50:27 +0000

    Mon, Jul 22, 2024, 12:50 AM +01:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,9 +1,9 @@
 -- SQL query to list the names of all people who starred in Toy Story
-SELECT DISTINCT COUNT(name) FROM people
+SELECT DISTINCT name FROM people
 WHERE people.id IN
 (
     SELECT person_id FROM directors
     JOIN ratings ON directors.movie_id = ratings.movie_id
     WHERE rating >= 9.0
 )
-;
+ORDER BY birth;
