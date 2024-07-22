380ba5a0ced3d7c277003ed6235c209ba53cbe91
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-22 10:31:43 +0000

    Mon, Jul 22, 2024, 11:31 AM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,7 +2,6 @@ SELECT title FROM movies
 WHERE id IN (
         SELECT movie_id FROM stars
         JOIN people ON person_id = people.id
-        WHERE name = 'Bradley Cooper'
-        AND name = 'Jennifer Lawrence'
+        WHERE name IN ('Bradley Cooper', 'Jennifer Lawrence')
     )
 ;
