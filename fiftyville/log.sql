b14fce74d7c6e8a6ecdfc8f51bc18a9fdc185374
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-22 17:48:29 +0000

    Mon, Jul 22, 2024, 6:48 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Find description of crime scene reports
-SELECT description FROM crime_scene_reports
+SELECT * FROM crime_scene_reports
 WHERE year = 2023
 AND month = 7
-LIMIT 5;
+AND street = 'Humphrey Street';
