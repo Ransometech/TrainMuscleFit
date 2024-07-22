4bf089c9b2bc31f1b7b26a6edfa71e6eba7a710e
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-22 10:44:09 +0000

    Mon, Jul 22, 2024, 11:44 AM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,4 @@
+--  SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred
 SELECT title FROM movies
 WHERE id IN (
         SELECT movie_id FROM stars
