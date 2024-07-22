de23b9d6cfdd67447161d2d769fbc1e227bed730
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-22 17:49:38 +0000

    Mon, Jul 22, 2024, 6:49 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,3 +5,4 @@ SELECT * FROM crime_scene_reports
 WHERE year = 2023
 AND month = 7
 AND street = 'Humphrey Street';
+ (/*)Humphrey Street | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
