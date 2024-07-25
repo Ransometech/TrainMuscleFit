5e0e377b3ec27d96fbff107c49adfd9a651d723f
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-25 19:55:29 +0000

    Thu, Jul 25, 2024, 8:55 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -28,8 +28,16 @@ def index():
 
         # TODO: Add the user's entry into the database
         name = request.form.get("name")
+        if not name:
+            return redirect("/")
+
         month = request.form.get("month")
+        if not month:
+            return redirect("/")
+
         day = request.form.get("day")
+        if not day:
+            return redirect("/")
         db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
