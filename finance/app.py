4405eaa5f89ed38f8941063aa2b9a0d356b3a51d
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-26 20:55:52 +0000

    Fri, Jul 26, 2024, 9:55 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -157,6 +157,45 @@ def login():
     else:
         return render_template("login.html")
 
+@app.route("/password_reset", methods=["GET", "POST"])
+def password_reset():
+    """Change user password"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        flash('Logged in!')
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
