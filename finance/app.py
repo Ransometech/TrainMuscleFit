4d78b7310e66a102917884557c3b7c8606eb08ed
Samson Ajadalu <64048938+Ransometech@users.noreply.github.com>
2024-07-26 10:44:54 +0000

    Fri, Jul 26, 2024, 11:44 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -46,10 +46,9 @@ def index():
     ''')
 
     index_portfolio = db.execute(
-            "SELECT * FROM portfolio JOIN users ON id = user_id WHERE id = ?",  session["user_id"])
+            "SELECT Symbol, Shares,  FROM portfolio JOIN users ON id = user_id WHERE id = ?",  session["user_id"])
         )
 
-                    )
     return render_template("index.html", portfolio = )
 
 
